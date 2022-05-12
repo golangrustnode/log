eb1eeb415878e0da0b8c501fbd14217af7ebd535
golangrustnode <hacklinux.org@gmail.com>
2022-05-12 10:42:40 +0800

    optimize log format

--- log.go
+++ log.go
@@ -2,18 +2,20 @@ package log
 
 import (
 	"github.com/sirupsen/logrus"
-	"path"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 func init()  {
 	logrus.SetReportCaller(true)
 	logrus.SetFormatter(&logrus.JSONFormatter{
-		TimestampFormat:"2006-01-02 15:03:04",
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
-			//处理文件名
-			fileName := path.Base(frame.File)
-			return frame.Function, fileName
+			//fileName := path.Base(frame.File) + ":" + strconv.Itoa(frame.Line)
+			//return frame.Function, fileName
+			sps := strings.Split(frame.File,"github.com")
+			len := len(sps)
+			return "", sps[len-1]+":"+strconv.Itoa(frame.Line)
 		},
 	})
 }
