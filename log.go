b025266b238e546b8b3d77934dd487946b4a9685
golangrustnode <hacklinux.org@gmail.com>
2024-09-04 11:28:42 +0800

    add env support

--- log.go
+++ log.go
@@ -2,35 +2,43 @@ package log
 
 import (
 	"github.com/sirupsen/logrus"
+	"os"
 	"runtime"
 	"strconv"
 	"strings"
 )
 
-func init()  {
+func init() {
 	logrus.SetReportCaller(true)
 	logrus.SetFormatter(&logrus.JSONFormatter{
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
 			//fileName := path.Base(frame.File) + ":" + strconv.Itoa(frame.Line)
 			//return frame.Function, fileName
-			sps := strings.Split(frame.File,"github.com")
+			sps := strings.Split(frame.File, "github.com")
 			len := len(sps)
-			return "", sps[len-1]+":"+strconv.Itoa(frame.Line)
+			return "", sps[len-1] + ":" + strconv.Itoa(frame.Line)
 		},
 	})
+	logLevel := os.Getenv("LOG_LEVEL")
+	level, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		logrus.SetLevel(logrus.InfoLevel) // 默认级别
+	} else {
+		logrus.SetLevel(level)
+	}
 }
 
-var Trace=logrus.Trace
-var Debug=logrus.Debug
-var Info =logrus.Info
-var Warn=logrus.Warn
-var Error=logrus.Error
-var Fatal=logrus.Fatal
-var Panic=logrus.Panic
+var Trace = logrus.Trace
+var Debug = logrus.Debug
+var Info = logrus.Info
+var Warn = logrus.Warn
+var Error = logrus.Error
+var Fatal = logrus.Fatal
+var Panic = logrus.Panic
 
-func SetLevel(level string)  {
+func SetLevel(level string) {
 	SetDebug()
-	l,err := logrus.ParseLevel(level)
+	l, err := logrus.ParseLevel(level)
 	if err != nil {
 		Debug(err)
 		return
@@ -41,21 +49,21 @@ func SetLevel(level string)  {
 func SetPanic() {
 	logrus.SetLevel(logrus.PanicLevel)
 }
-func SetFatal()  {
+func SetFatal() {
 	logrus.SetLevel(logrus.FatalLevel)
 }
-func SetError()  {
+func SetError() {
 	logrus.SetLevel(logrus.ErrorLevel)
 }
-func SetWarn()  {
+func SetWarn() {
 	logrus.SetLevel(logrus.WarnLevel)
 }
-func SetInfo()  {
+func SetInfo() {
 	logrus.SetLevel(logrus.InfoLevel)
 }
-func SetDebug()  {
+func SetDebug() {
 	logrus.SetLevel(logrus.DebugLevel)
 }
-func SetTrace()  {
+func SetTrace() {
 	logrus.SetLevel(logrus.TraceLevel)
 }
