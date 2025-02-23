3eb2516efd68579768c78243151dee0eb2e42a8d
golangrustnode <hacklinux.org@gmail.com>
2025-02-23 18:11:09 +0800

    print logs when add hook

--- cushook.go
+++ cushook.go
@@ -1,9 +1,14 @@
 package log
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 )
 
 func AddHook(hook logrus.Hook) {
-	logger.AddHook(hook)
+	if logger != nil {
+		logger.AddHook(hook)
+		fmt.Println("add hook", hook)
+	}
+	fmt.Println("logger is nil")
 }
